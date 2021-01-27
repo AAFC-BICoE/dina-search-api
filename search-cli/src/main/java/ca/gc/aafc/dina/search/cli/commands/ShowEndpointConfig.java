5a8cd0cb76137ba43e2a57031f656425f36e3f38
Christian Gendreau <christiangendreau@gmail.com>
2021-01-26 21:04:11 -0500

    21476 Add config file for type resolution Minor adjustments

--- search-cli/src/main/java/ca/gc/aafc/dina/search/cli/commands/ShowEndpointConfig.java
+++ search-cli/src/main/java/ca/gc/aafc/dina/search/cli/commands/ShowEndpointConfig.java
@@ -1,6 +1,5 @@
 package ca.gc.aafc.dina.search.cli.commands;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.stereotype.Component;
@@ -11,9 +10,9 @@ import ca.gc.aafc.dina.search.cli.config.ServiceEndpointProperties;
 @ShellComponent
 public class ShowEndpointConfig {
 
-  private ServiceEndpointProperties svcEndpointProps;
+  private final ServiceEndpointProperties svcEndpointProps;
 
-  public ShowEndpointConfig(@Autowired ServiceEndpointProperties svcEndpointProps) {
+  public ShowEndpointConfig(ServiceEndpointProperties svcEndpointProps) {
     this.svcEndpointProps = svcEndpointProps;
   }
 
@@ -23,7 +22,7 @@ public class ShowEndpointConfig {
     StringBuilder aBuilder = new StringBuilder();
     aBuilder.append(System.lineSeparator());
     aBuilder.append("****** Endpoints Configuration ******"  + System.lineSeparator());
-    svcEndpointProps.getEndpoints().entrySet().stream().forEach(e-> aBuilder.append(e.getKey() + "=" + e.getValue() + System.lineSeparator()));
+    svcEndpointProps.getEndpoints().entrySet().forEach(e-> aBuilder.append(e.getKey() + "=" + e.getValue() + System.lineSeparator()));
     aBuilder.append("*************************************"  + System.lineSeparator());
 
     return aBuilder.toString();
--- search-cli/src/main/java/ca/gc/aafc/dina/search/cli/config/ServiceEndpointProperties.java
+++ search-cli/src/main/java/ca/gc/aafc/dina/search/cli/config/ServiceEndpointProperties.java
@@ -2,19 +2,19 @@ package ca.gc.aafc.dina.search.cli.config;
 
 import java.util.Map;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Data
 @Component
 @ConfigurationProperties
 @PropertySource(value = "classpath:endpoints.yml", factory = YamlPropertySourceFactory.class)
-@PropertySource(value = "file:config/endpoints.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
+@RequiredArgsConstructor
+@Getter
 public class ServiceEndpointProperties {
-  
-  private Map<String, String> endpoints;
-
+  private final Map<String, String> endpoints;
 }
