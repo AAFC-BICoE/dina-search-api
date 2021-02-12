f645fa52b67205ebfdc24d51c041326a47da93ff
ydorego <ydorego@gmail.com>
2021-02-12 03:19:49 -0500

    Addition of IndexDocument command

--- search-cli/src/main/java/ca/gc/aafc/dina/search/cli/commands/IndexDocument.java
+++ search-cli/src/main/java/ca/gc/aafc/dina/search/cli/commands/IndexDocument.java
@@ -8,31 +8,34 @@ import org.springframework.stereotype.Component;
 import ca.gc.aafc.dina.search.cli.config.ServiceEndpointProperties;
 import ca.gc.aafc.dina.search.cli.exceptions.SearchApiException;
 import ca.gc.aafc.dina.search.cli.http.OpenIDHttpClient;
+import ca.gc.aafc.dina.search.cli.indexing.Indexer;
 import ca.gc.aafc.dina.search.cli.json.IndexableDocumentHandler;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 @ShellComponent
-public class GetDocument {
+public class IndexDocument {
 
   private final OpenIDHttpClient aClient;
   private final ServiceEndpointProperties svcEndpointProps;
   private final IndexableDocumentHandler indexableDocumentHandler;
-  
-  public GetDocument(OpenIDHttpClient aClient, ServiceEndpointProperties svcEndpointProps,
-              IndexableDocumentHandler indexableDocumentHandler) {
+  private final Indexer indexer;
+
+  public IndexDocument(OpenIDHttpClient aClient, ServiceEndpointProperties svcEndpointProps,
+              IndexableDocumentHandler indexableDocumentHandler,
+              Indexer indexer) {
 
     this.aClient = aClient;
     this.svcEndpointProps = svcEndpointProps;
     this.indexableDocumentHandler = indexableDocumentHandler;
+    this.indexer = indexer;
   }
 
-  @ShellMethod(value = "Get Document from a specified endpoint", key = "get-document")
-  public String getDocument(
+  @ShellMethod(value = "Index a document into elasticsearch", key = "index-document")
+  public String indexDocument(
                   @ShellOption(help = "Document type", value = { "-t", "--type" }) String type,
-                  @ShellOption(help = "Unique object identifier", value = { "-i", "--documentId" }) String documentId,
-                  @ShellOption(help = "Assemble a document", defaultValue = "false", value = "--assemble" ) boolean assemble) {
+                  @ShellOption(help = "Unique object identifier", value = { "-i", "--documentId" }) String documentId) {
 
     String msg = null;
     if (!svcEndpointProps.getEndpoints().containsKey(type)) {
@@ -42,11 +45,19 @@ public class GetDocument {
     }
 
     try {
+
+      // Step #1: get the document
+      log.info("Retrieve document id:{}", documentId);
       msg = aClient.getDataFromUrl(svcEndpointProps.getEndpoints().get(type), documentId);
 
-      if (assemble) {
-        msg = indexableDocumentHandler.assembleDocument(msg);
-      }
+      // Step #2: Assemble the document
+      log.info("Assemble document id:{}", documentId);
+      msg = indexableDocumentHandler.assembleDocument(msg);
+
+      // Step #3: Index the document into elasticsearch
+      log.info("Sending document id:{} to indexer", documentId);
+      indexer.indexDocument(msg);
+
     } catch (SearchApiException sapiEx) {
       log.error("Error during operation execution", sapiEx);
     }
