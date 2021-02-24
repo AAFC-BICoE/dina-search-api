f6d3912892e8ad3ac523fb2b0cd55b7c87b12b04
ydorego <ydorego@gmail.com>
2021-02-24 09:13:58 -0500

    Initial commit for search-ws artifacts and controllers

---
+++ search-ws/src/main/java/ca/gc/aafc/dina/search/ws/exceptions/SearchApiException.java
@@ -0,0 +1,18 @@
+package ca.gc.aafc.dina.search.ws.exceptions;
+
+public class SearchApiException extends Exception {
+
+  private static final long serialVersionUID = 987750562339932511L;
+
+  public SearchApiException(String message) {
+    super(message);
+  }
+
+  public SearchApiException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public SearchApiException(Throwable cause) {
+    super(cause);
+  }
+}
