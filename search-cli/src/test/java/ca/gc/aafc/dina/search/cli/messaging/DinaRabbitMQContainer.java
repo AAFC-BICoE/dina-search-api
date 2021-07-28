e12b41aa756cf0071af09785be0eb0a795d778d4
ydorego <ydorego@gmail.com>
2021-07-28 12:23:36 -0400

    Add integration tests for messaging

---
+++ search-cli/src/test/java/ca/gc/aafc/dina/search/cli/messaging/DinaRabbitMQContainer.java
@@ -0,0 +1,16 @@
+package ca.gc.aafc.dina.search.cli.messaging;
+
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class DinaRabbitMQContainer extends RabbitMQContainer {
+
+  private static final DockerImageName myImage = DockerImageName.parse("rabbitmq:3-management-alpine");
+
+  public DinaRabbitMQContainer() {
+    super(myImage);
+
+    this.addFixedExposedPort(5672, 5672);
+    this.addFixedExposedPort(15672, 15672);
+  } 
+}
