057a2d09ec646b2ecdda44840da85882b5346295
Christian Gendreau <christiangendreau@gmail.com>
2021-12-22 09:50:30 -0500

    26078 Reduce payload on autocomplete fixed Checkstyle

--- search-ws/src/main/java/ca/gc/aafc/dina/search/ws/services/ESSearchService.java
+++ search-ws/src/main/java/ca/gc/aafc/dina/search/ws/services/ESSearchService.java
@@ -3,7 +3,6 @@ package ca.gc.aafc.dina.search.ws.services;
 import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
