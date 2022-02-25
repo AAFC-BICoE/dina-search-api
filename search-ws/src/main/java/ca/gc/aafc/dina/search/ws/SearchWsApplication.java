411b9e8d68c1310ba886e83b1427d25a230f21a1
Brandon Andre <andr0284@algonquinlive.com>
2022-02-25 16:03:54 -0500

    Support #26093 - Add dina base to enable authentication (#71)
    - Added dina-base api to the pom.
    - Added KeycloakAuth and KeycloakDisabledAuth configurations from dina base.
    - Exclude the data source / javers stuff.
    - Added keycloak to application yml.
    - Removed ApplicationSecurity since Keycloak configuration is now handling this.

--- search-ws/src/main/java/ca/gc/aafc/dina/search/ws/SearchWsApplication.java
+++ search-ws/src/main/java/ca/gc/aafc/dina/search/ws/SearchWsApplication.java
@@ -1,13 +1,21 @@
 package ca.gc.aafc.dina.search.ws;
 
+import org.javers.spring.boot.sql.JaversSqlAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import ca.gc.aafc.dina.security.KeycloakAuthConfig;
+import ca.gc.aafc.dina.security.KeycloakDisabledAuthConfig;
 
 // CHECKSTYLE:OFF HideUtilityClassConstructor (Configuration class can not have
 // invisible constructor, ignore the check style error for this case)
 @SpringBootApplication
-@EnableAutoConfiguration
+@ImportAutoConfiguration(
+  classes = {KeycloakAuthConfig.class, KeycloakDisabledAuthConfig.class}, 
+  exclude = {DataSourceAutoConfiguration.class, JaversSqlAutoConfiguration.class}
+)
 public class SearchWsApplication {
 
   public static void main(String[] args) {
