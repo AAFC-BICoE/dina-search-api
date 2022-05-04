f077ecd74e4ade9202e5840265204f1d1d0c5f58
Christian Gendreau <christiangendreau@gmail.com>
2022-05-04 15:57:11 -0400

    27486 Allow configuration to store disctinct_term_agg Fixed Checkstyle issues

--- search-ws/src/main/java/ca/gc/aafc/dina/search/ws/services/IndexMappingResponse.java
+++ search-ws/src/main/java/ca/gc/aafc/dina/search/ws/services/IndexMappingResponse.java
@@ -1,6 +1,5 @@
 package ca.gc.aafc.dina.search.ws.services;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
@@ -19,12 +18,12 @@ public class IndexMappingResponse {
   @Builder
   @JsonInclude(JsonInclude.Include.NON_NULL)
   static class Attribute {
-    final String name;
-    final String type;
-    final String path;
+    private final String name;
+    private final String type;
+    private final String path;
 
     @JsonProperty("distinct_term_agg")
-    final Boolean distinctTermAgg;
+    private final Boolean distinctTermAgg;
 
     private Attribute(String name, String type, String path, Boolean distinctTermAgg) {
       this.name = name;
