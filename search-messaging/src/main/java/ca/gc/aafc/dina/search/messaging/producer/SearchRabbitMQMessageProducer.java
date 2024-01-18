044ef3c2d4a5395557dd3b0c855ccfe0c5568d31
Christian Gendreau <christiangendreau@gmail.com>
2024-01-18 15:01:44 -0500

    Renamed provider to avoid name conflict

--- search-messaging/src/main/java/ca/gc/aafc/dina/search/messaging/producer/SearchRabbitMQMessageProducer.java
+++ search-messaging/src/main/java/ca/gc/aafc/dina/search/messaging/producer/SearchRabbitMQMessageProducer.java
@@ -1,6 +1,7 @@
 package ca.gc.aafc.dina.search.messaging.producer;
 
 import ca.gc.aafc.dina.messaging.config.RabbitMQQueueProperties;
+import ca.gc.aafc.dina.messaging.producer.RabbitMQMessageProducer;
 import ca.gc.aafc.dina.search.messaging.types.DocumentOperationNotification;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,10 @@ import javax.inject.Named;
  */
 @Service
 @ConditionalOnProperty(prefix = "dina.messaging", name = "isProducer", havingValue = "true")
-public class RabbitMQMessageProducer extends ca.gc.aafc.dina.messaging.producer.RabbitMQMessageProducer<DocumentOperationNotification> implements MessageProducer {
+public class SearchRabbitMQMessageProducer extends RabbitMQMessageProducer<DocumentOperationNotification> implements MessageProducer {
 
   @Autowired
-  public RabbitMQMessageProducer(RabbitTemplate rabbitTemplate, @Named("searchQueueProperties") RabbitMQQueueProperties queueProperties) {
+  public SearchRabbitMQMessageProducer(RabbitTemplate rabbitTemplate, @Named("searchQueueProperties") RabbitMQQueueProperties queueProperties) {
     super(rabbitTemplate, queueProperties);
   }
 }
